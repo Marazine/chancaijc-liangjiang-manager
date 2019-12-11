0576122d0d6ecf2b8a6d7847c67994835ff24681
liwenhao <lwh2011@126.com>
2019-12-11 10:15:50 +0800

    增加日志查询条件, 优化查看详情样式

--- src/views/modules/sys/log-info.vue
+++ src/views/modules/sys/log-info.vue
@@ -1,38 +1,25 @@
 <template>
-  <el-dialog
-    title="日志详情"
-    :close-on-click-modal="false"
-    :visible.sync="visible"
-  ><el-row>
-    <el-col :md="9" :sm="11" :push="1" v-if="dataForm.user">
-        <div class="itemBox">
-            <el-col :md="5" :sm="5" class="title">姓名</el-col>
-            <el-col :md="1" :sm="1" class="title title_left">:</el-col>
-            <el-col :md="18" :sm="18" class="content">{{dataForm.user}}</el-col>
-        </div>
-        <div class="itemBox">
-            <el-col :md="5" :sm="5" class="title">id</el-col>
-            <el-col :md="1" :sm="1" class="title title_left">:</el-col>
-            <el-col :md="18" :sm="18" class="content">{{dataForm.id}}</el-col>
-        </div>
-        <div class="itemBox">
-            <el-col :md="5" :sm="5" class="title">ip</el-col>
-            <el-col :md="1" :sm="1" class="title title_left">:</el-col>
-            <el-col :md="18" :sm="18" class="content">{{dataForm.ip}}</el-col>
-        </div>
-        <div class="itemBox">
-            <el-col :md="5" :sm="5" class="title">操作时间</el-col>
-            <el-col :md="1" :sm="1" class="title title_left">:</el-col>
-            <el-col :md="18" :sm="18" class="content">{{dataForm.createtime}}</el-col>
-        </div>
-    </el-col>
-    <el-col :md="9" :sm="11" :push="1" v-if="dataForm.id">
-        <div class="itemBox">
-            <el-col :md="5" :sm="5" class="title">操作内容</el-col>
-            <el-col :md="1" :sm="1" class="title title_left">:</el-col>
-            <el-col :md="18" :sm="18" class="content">{{dataForm.extendInfo}}</el-col>
-        </div>
-    </el-col>
+  <el-dialog title="日志详情" :close-on-click-modal="false" :visible.sync="visible">
+    <el-row>
+      <el-col :span="10" v-if="dataForm.user">
+        <el-col :span="6" class="">姓名: </el-col>
+        <el-col :span="18" class="">{{dataForm.user}}</el-col>
+        <el-col :span="6" class="">id: </el-col>
+        <el-col :span="18" class="">{{dataForm.id}}</el-col>
+        <el-col :span="6" class="">ip: </el-col>
+        <el-col :span="18" class="">{{dataForm.ip}}</el-col>
+        <el-col :span="6" class="">操作时间: </el-col>
+        <el-col :span="18" class="">{{dataForm.createtime}}</el-col>
+      </el-col>
+      <el-col :span="14" v-if="dataForm.id">
+        <el-col :span="24" class="">操作内容: </el-col>
+        <el-col class="">
+          <div class="my-postParams" style="marginTop: 20px">postParams: </div>
+          <json-viewer :value="postParams"></json-viewer>
+          <div>postResult: </div>
+          <json-viewer :value="postResult"></json-viewer>
+        </el-col>
+      </el-col>
     </el-row>
     <span slot="footer" class="dialog-footer">
       <el-button @click="visible = false">取消</el-button>
@@ -41,7 +28,11 @@
 </template>
 
 <script>
+import JsonViewer from 'vue-json-viewer'
 export default {
+  components: {
+    'json-viewer': JsonViewer
+  },
   data() {
     return {
       dataForm: {
@@ -54,6 +45,8 @@ export default {
         ip: "",
         createtime:""
       },
+      postParams: {},
+      postResult: {},
       visible : false
     };
   },
@@ -67,6 +60,10 @@ export default {
       }).then(({ data }) => {
         if (data && data.code == 200) {
           this.dataForm = data.data;
+          let postParams = this.dataForm.extendInfo.indexOf('postParams');
+          let postResult = this.dataForm.extendInfo.indexOf('postResult');
+          this.postParams = JSON.parse(this.dataForm.extendInfo.slice(postParams+11, postResult-1) || "{}");
+          this.postResult = JSON.parse(this.dataForm.extendInfo.slice(postResult+11) || "{}");
         }
         this.visible = true;
       });
--- src/views/modules/sys/log.vue
+++ src/views/modules/sys/log.vue
@@ -1,73 +1,55 @@
 <template>
   <div class="mod-user">
-    <el-table
-      :data="dataList"
-      border
-      v-loading="dataListLoading"
-      style="width: 100%;">
-      <el-table-column
-        type="selection"
-        header-align="center"
-        align="center"
-        width="50">
-      </el-table-column>
-      <el-table-column
-        prop="operatorId"
-        header-align="center"
-        align="center"
-        label="操作人ID">
-      </el-table-column>
-      <el-table-column
-        prop="user"
-        header-align="center"
-        align="center"
-        label="操作人">
-      </el-table-column>
-      <el-table-column
-        prop="userType"
-        header-align="center"
-        align="center"
-        label="操作平台">
+    <!-- 表单搜索 START -->
+    <el-form :inline="true" class="">
+        <el-form-item>
+            <el-input v-model="condition.user" placeholder="操作人" clearable></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-input v-model="condition.ip" placeholder="ip" clearable></el-input>
+        </el-form-item>
+        <el-form-item>
+            <el-select  v-model="condition.userType" filterable clearable placeholder="操作平台">
+              <el-option label="分析系统" value="1"></el-option>
+              <el-option label="问卷系统" value="2"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item class="my-form">
+            <el-date-picker v-model="time"
+                            style="width: 70%"
+                            type="daterange"
+                            range-separator="-"
+                            start-placeholder="开始日期"
+                            end-placeholder="结束日期"
+                            format="yyyy-MM-dd"
+                            value-format="yyyy-MM-dd">
+            </el-date-picker>
+        </el-form-item>
+        <el-form-item style="marginLeft: -115px;">
+            <el-button type="primary" @click="serchData()" >查询</el-button>
+        </el-form-item>
+    </el-form>
+    <!-- 表单搜索 END -->
+    <el-table :data="dataList" border v-loading="dataListLoading" style="width: 100%;">
+      <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
+      <el-table-column prop="operatorId" header-align="center" align="center" label="操作人ID"></el-table-column>
+      <el-table-column prop="user" header-align="center" align="center" label="操作人"></el-table-column>
+      <el-table-column prop="userType" header-align="center" align="center" label="操作平台">
         <template slot-scope="scope">
-          <span v-if="scope.row.userType == 1">分析系統</span>
-          <span v-else>問卷系統</span>
+          <span v-if="scope.row.userType == 2">问卷系统</span>
+          <span v-else>分析系统</span>
         </template>
       </el-table-column>
-      <!--<el-table-column
-        prop="operatorType"
-        header-align="center"
-        align="center"
-        label="操作类型">
+      <!--<el-table-column prop="operatorType" header-align="center" align="center" label="操作类型">
         <template slot-scope="scope">
-        <el-tag v-if="scope.row.operatorType === 0" size="small" type="danger"></el-tag>
-        <el-tag v-else size="small">正常</el-tag>
+          <el-tag v-if="scope.row.operatorType === 0" size="small" type="danger"></el-tag>
+          <el-tag v-else size="small">正常</el-tag>
         </template>
       </el-table-column>-->
-      <el-table-column
-        prop="operationDesc"
-        header-align="center"
-        align="center"
-        label="操作描述">
-      </el-table-column>
-      <el-table-column
-        prop="ip"
-        header-align="center"
-        align="center"
-        label="ip">
-      </el-table-column>
-      <el-table-column
-        prop="createtime"
-        header-align="center"
-        align="center"
-        width="180"
-        label="操作时间">
-      </el-table-column>
-      <el-table-column
-        fixed="right"
-        header-align="center"
-        align="center"
-        width="150"
-        label="操作">
+      <el-table-column prop="operationDesc" header-align="center" align="center" label="操作描述"></el-table-column>
+      <el-table-column prop="ip" header-align="center" align="center" label="ip"></el-table-column>
+      <el-table-column prop="createtime" header-align="center" align="center" width="180" label="操作时间"></el-table-column>
+      <el-table-column fixed="right" header-align="center" align="center" width="150" label="操作">
         <template slot-scope="scope">
           <el-button v-if="isAuth('sys:log:info')" type="text" size="small" @click="addOrUpdateHandle(scope.row.admin_id)">修改</el-button>
         </template>
@@ -76,7 +58,6 @@
           <el-button v-if="isAuth('sys:log:info')" type="text" size="small" @click="infoHandle(scope.row.id)">查看详情</el-button>
         </template>
       </el-table-column>
-
     </el-table>
     <el-pagination
       @size-change="sizeChangeHandle"
@@ -118,7 +99,15 @@ export default {
       dataListLoading: false,
       dataListSelections: [],
       addOrUpdateVisible: false,
-      infoVisible: false
+      infoVisible: false,
+      time: [],
+      condition: {
+        user: '',
+        ip: '',
+        userType: '',
+        startTime: '',
+        endTime: '',
+      }
     };
   },
   components: {
@@ -131,6 +120,20 @@ export default {
   activated() {
     this.getDataList()
   },
+  watch: {
+    time: {
+      handler: function (val, oldVal) { 
+        if(val == null) {
+          this.condition.startTime = '';
+          this.condition.endTime = '';
+        } else {
+          this.condition.startTime = val[0];
+          this.condition.endTime = val[1];
+        }
+      },
+      deep: true
+    },
+  },
   methods: {
     // 获取数据列表
     getDataList() {
@@ -138,7 +141,8 @@ export default {
       let url = this.$http.adornUrl("/operator/list", "CQ"),
       param = {
         pageNo: this.pageIndex,
-        pageSize: this.pageSize
+        pageSize: this.pageSize,
+        condition: JSON.stringify(this.condition)
       }
       this.$http({
         url: url,
@@ -147,13 +151,19 @@ export default {
       }).then(({ data }) => {
         if (data && data.code == 200) {
           this.dataList = data.data;
-          this.totalPage = parseInt(data.pageparam.sums);
+          this.totalPage = data.pageparam.sums;
+          this.pageIndex = data.pageparam.page;
+          this.pageSize = data.pageparam.page_size;
         } else {
           this.dataList = [];
           this.totalPage = 0;
         }
         this.dataListLoading = false;
       });
+    },
+    serchData() {
+      this.pageIndex = 1;
+      this.getDataList();
     },
     // 每页数
     sizeChangeHandle(val) {
@@ -183,3 +193,15 @@ export default {
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.mod-user {
+  .my-form {
+      .el-form-item__content {
+          .el-input__inner {
+              padding-right: 4px;
+          }
+      }
+  }
+}
+</style>
