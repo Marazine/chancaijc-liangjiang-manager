7eeb4136bf011ce8fefc444ae34e5e781222b690
xiaocn325 <1639270979@qq.com>
2019-12-17 11:54:09 +0800

    xiao

--- src/views/board/components/double_bar.vue
+++ src/views/board/components/double_bar.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="bh-100">
-        <div class="chart-wrap" ref="pillar"></div>
-    </div>
+  <div class="bh-100">
+    <div class="chart-wrap" ref="pillar"></div>
+  </div>
 </template>
 
 <script>
     export default {
         data() {
             return {
-	    	myChart:null,
+                myChart: null,
                 chartData: {
                     "name": ['海洋产业', '航天航空产业', '生物产业', '新材料产业', '节能环保产业', '生命健康产业', '新能源产业', '文化创意产业', '高端装备制造',
                         '现代服务产业', '互联网产业', '新一代信息技术产业'
@@ -43,25 +43,25 @@
 
                 let option = {
                     tooltip: {
-        trigger: 'axis',
-        axisPointer: {
-            type: 'shadow'
-        }
-    },
+                        trigger: 'axis',
+                        axisPointer: {
+                            type: 'shadow'
+                        }
+                    },
                     xAxis: [{
                         data: data_chart.name,
                         axisLabel: {
                             interval: 0,
                             show: false
                         },
-                    },{
+                    }, {
                         data: data_chart.name,
                         axisLabel: {
                             interval: 0,
                             show: true
                         },
                         axisLine: {
-                        show: false  
+                            show: false
                         },
                         axisTick: {
                             show: false
@@ -70,195 +70,84 @@
                     }],
                     yAxis: [{
                         type: 'value',
-                        show: true
-                    },{
+                        show: true,
+                        axisLine: {
+                            show: false
+                        },
+                        axisTick: {
+                            show: true,
+                            lineStyle: {
+                                color: "#a0c0ff",
+                                with: 3
+                            }
+                        },
+                        axisLabel: {
+                            show: true,
+                            normal: {
+                                color: "#fff"
+                            }
+                        },
+                    }, {
                         gridIndex: 1,
                         show: false,
                     }],
                     grid: [{
                         width: '80%',
                         left: '10%'
-                    },{
+                    }, {
                         left: '13.3%',
                         width: '80%'
                     }],
-                    series: [
-                        {
-                            type: 'bar',
-                            barWidth: 10,
-                            data: data_chart.value,
-                            itemStyle: {
-                                barBorderRadius: 10,
-                                color: {
-                                    type: 'linear',
-                                    x: 0,
-                                    y: 0,
-                                    x2: 0,
-                                    y2: 1,
-                                    colorStops: [{
-                                        offset: 0, color: '#37FFF9' // 0% 处的颜色
-                                    }, {
-                                        offset: 1, color: 'rgba(0, 222, 215, 0.2)' // 100% 处的颜色
-                                    }],
-                                }
+                    series: [{
+                        type: 'bar',
+                        barWidth: 10,
+                        data: data_chart.value,
+                        itemStyle: {
+                            barBorderRadius: 10,
+                            color: {
+                                type: 'linear',
+                                x: 0,
+                                y: 0,
+                                x2: 0,
+                                y2: 1,
+                                colorStops: [{
+                                    offset: 0,
+                                    color: '#37FFF9' // 0% 处的颜色
+                                }, {
+                                    offset: 1,
+                                    color: 'rgba(0, 222, 215, 0.2)' // 100% 处的颜色
+                                }],
                             }
-                            
-                        },
-                        {
-                            type: 'bar',
-                            barWidth: 10,
-                            data: data_chart.value1,
-                            itemStyle: {
-                                barBorderRadius: 10,
-                                color: {
-                                    type: 'linear',
-                                    x: 0,
-                                    y: 0,
-                                    x2: 0,
-                                    y2: 1,
-                                    colorStops: [{
-                                        offset: 0, color: '#FF5624' // 0% 处的颜色
-                                    }, {
-                                        offset: 1, color: 'rgba(255, 86, 36, 0.2)' // 100% 处的颜色
-                                    }],
-                                }
-                            }
-                        },
-                    ]
-                };
-                
-                // {
-                //     backgroundColor: 'transparent',
-                    // tooltip: {
-                    //     trigger: 'axis',
-                    //     axisPointer: { // 坐标轴指示器，坐标轴触发有效
-                    //         type: "shadow" // 默认为直线，可选为：'line' | 'shadow'
-                    //     },
-                    //     formatter: function(params) {
-                    //         let time = '';
-                    //         let str = '';
-                    //         for (let i of params) {
-                    //             time = i.name.replace(/\n/g, '') + '<br />';
-                    //             if (i.data == 'null' || i.data == null) {
-                    //                 str += i.seriesName + '：无数据' + '<br />'
-                    //             } else {
-                    //                 str += i.seriesName + '：' + Math.abs(i.data) + '<br />'
-                    //             }
-                    //         }
-                    //         return time + str;
-                    //     },
-                    // },
-                    // color: [new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-                    //         offset: 0,
-                    //         color: "#0167e8"
-                    //     }, {
-                    //         offset: 1,
-                    //         color: "#13ace8"
-                    //     }], false),
-                    //     new this.$echarts.graphic.LinearGradient(0, 0, 1, 0, [{
-                    //         offset: 0,
-                    //         color: "#963ff1"
-                    //     }, {
-                    //         offset: 1,
-                    //         color: "#fd5c8c"
-                    //     }], false)
-                    // ],
-                    // legend: {
-                    //     top: 12,
-                    //     itemGap: 10,
-                    //     itemWidth: 10,
-                    //     itemHeight: 10,
-                    //     data: this.config.legend
-                    // },
-                    // grid: {
-                    //     left: '4%',
-                    //     right: '10%',
-                    //     bottom: '-8%',
-                    //     top: '0',
-                    //     containLabel: true
-                    // },
-                    // // xAxis: [ {
-                    // //     type : 'value'
-                    // // }],
-                    // xAxis: {
-                    //     show: false
-                    // },
-                    // yAxis : [
-                    //     {
-                    //         type : 'category',
-                    //         axisLine: {
-                    //             show: false
-                    //         },
-                    //         axisTick: {
-                    //             show: false
-                    //         },
-                    //         axisLabel: {
-                    //             show: true,
-                    //             interval: '0',
-                    //             // margin: this.config.margin,
-                    //             // align: "left",
-                    //             textStyle: {
-                    //                 fontSize: 11,
-                    //                 color: '#ddd',
-                    //             },
-                    //             formatter: function(value) {
-                    //                 if (value.length > 8) {
-                    //                     return value.substring(0, 8) + "..";
-                    //                 } else {
-                    //                     return value;
-                    //                 }
-                    //             },
-                    //         },
-                    //         // data : ['分拣','清洗','抛光','研磨','脱膜','切割','压膜','压膜分配','光固化后处理','光固化']
-                    //         data : data_chart.name
-                    //     }
-                    // ],
-                    // series : [
-                    //     {
-                    //         name: this.config.legend[0],
-                    //         type:'bar',
-                    //         stack: '总量',
-                    //         // barWidth: 12,
-                    //         label: {
-                    //             // normal: {
-                    //             //     // show: true,
-                    //             //     // position: 'left',
-                    //             //     // color: '#ddd',
-                    //             //     // fontSize: '10',
-                    //             //     // formatter: function(params) {
-                    //             //     //     return params.data * -1;
-                    //             //     // }
-                    //             // }
-                    //         },
-                    //         // data:[400, 241,360, 320, 302, 341, 374, 390, 450, 420]
-                    //         data: data_chart.value
-                    //     },
-                    //     {
-                    //         name: this.config.legend[1],
-                    //         type:'bar',
-                    //         stack: '总量',
-                    //         // barWidth: 12,
-                    //         label: {
-                    //             // normal: {
-                    //             //     // formatter: function(params){return -params.value}
-                    //             //     // normal: {
-                    //             //         // show: true,
-                    //             //         // // position: 'right',
+                        }
 
-                    //             //         // color: '#ddd',
-                    //             //         // fontSize: '10',
-                    //             //     // }
-                    //             // }
-                    //         },
-                    //         // data:[-120, -180,-120, -120, -132, -101, -134, -190, -230, -210]
-                    //         data: data_chart.value1
-                    //     }
-                    // ],
-                // };
+                    }, {
+                        type: 'bar',
+                        barWidth: 10,
+                        data: data_chart.value1,
+                        itemStyle: {
+                            barBorderRadius: 10,
+                            color: {
+                                type: 'linear',
+                                x: 0,
+                                y: 0,
+                                x2: 0,
+                                y2: 1,
+                                colorStops: [{
+                                    offset: 0,
+                                    color: '#FF5624' // 0% 处的颜色
+                                }, {
+                                    offset: 1,
+                                    color: 'rgba(255, 86, 36, 0.2)' // 100% 处的颜色
+                                }],
+                            }
+                        }
+                    }, ]
+                };
+
 
                 this.myChart.setOption(option);
-                window.addEventListener("resize", ()=> {
-                    if(this.myChart) {
+                window.addEventListener("resize", () => {
+                    if (this.myChart) {
                         this.myChart.resize();
                     }
                 })
--- src/views/board/rcld/rcld.vue
+++ src/views/board/rcld/rcld.vue
@@ -5,140 +5,138 @@
       <div class="title-text">
         <img src="../../../assets/img/rcld.png" alt="">
       </div>
-      <img class="fuseChildrenBack" @click="$router.push({name:'board'})"
-        src="../../../assets/img/0001.png" alt="">
+      <img class="fuseChildrenBack" @click="$router.push({name:'board'})" src="../../../assets/img/0001.png" alt="">
       <div class="row bh-100">
-          <div class="row col-md-12 columnBox ">
-            <div class="columnBottom row">
-              <div class=" col-md-6 column column100">
-                <Chart :initData="pie_aData" :config='pie_a_year_config'></Chart>
-              </div>
-              <div class=" col-md-6 column column100">
-                <Chart :initData="pie_a_nextData" :config='pie_a_next_year_config'></Chart>
-              </div>
+        <div class="row col-md-12 columnBox ">
+          <div class="columnBottom row">
+            <div class=" col-md-6 column column100">
+              <Chart :initData="pie_aData" :config='pie_a_year_config'></Chart>
             </div>
-            <div class="columnBottom row">
-              <div class=" col-md-6 column column100">
-                <Chart :initData="estate_salary_Data" :config='estate_salary_config'></Chart>
-              </div>
-              <div class=" col-md-6 column column100">
-                <Chart :initData="estateData" :config='estate_config'></Chart>
-              </div>
+            <div class=" col-md-6 column column100">
+              <Chart :initData="pie_a_nextData" :config='pie_a_next_year_config'></Chart>
             </div>
           </div>
+          <div class="columnBottom row">
+            <div class=" col-md-6 column column100">
+              <Chart :initData="estate_salary_Data" :config='estate_salary_config'></Chart>
+            </div>
+            <div class=" col-md-6 column column100">
+              <Chart :initData="estateData" :config='estate_config'></Chart>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import double_pillar from "../component/double_pillar" 
+    import double_pillar from "../component/double_pillar"
 
-export default {
-    components: {
-      double_pillar
-    },
-    data() {
-        return {
-            isShow: false,
-            pie_aData: {},
-            pie_a_year_config: {
-              type: 'bussinessFlow',
-              title: "十三五重点行业2020年度国内社会人才需求",
-              type: 'bussinessFlow',
-              isScroll: true,
-              paddingTop: '8%',
-              c_left: "5%",
-              margin: '5%',
-              legend: ['省内', '省外']
-            },
-            pie_a_nextData: {},
-            pie_a_next_year_config: {
-                type: 'industuryFlow',
-                title: "各产业2020年度国内应届生需求",
-                paddingTop: '8%',
-                radius: ['35%', '50%'],
-                radius2: [0, '32%'],
-                left: '20%',
-                right: '20%',
-                top: '0',
-                bottom: '0',
-            },
-            estate_salary_Data: {},
-            estate_salary_config: {
-                type: 'natureFlow',
-                title: '各产业2020年度外籍人才需求',
-                paddingTop: '8%',
-                echartTitleShow: false,
-                left: '4%',
-                right: '10%',
-                top: '10%',
-                bottom: '-8%',
-                bh: 'bh-50',
-                isClick: false,
-            },
-            estateData: {},
-            estate_config: {
-                type: 'lsl',
-                title: '各产业2020年度国留学归国人才需求',
-                radius: ['40%', '80%'],
-                paddingTop: '6%',
-                top: '15%',
-                right: '8%',
-                left: '12%',
-                bottom: '20%',
+    export default {
+        components: {
+            double_pillar
+        },
+        data() {
+            return {
+                isShow: false,
+                pie_aData: {},
+                pie_a_year_config: {
+                    type: 'bussinessFlow',
+                    title: "十三五重点行业2020年度国内社会人才需求",
+                    isScroll: false,
+                    paddingTop: '8%',
+                    c_left: "5%",
+                    margin: '5%',
+                    legend: ['省内', '省外']
+                },
+                pie_a_nextData: {},
+                pie_a_next_year_config: {
+                    type: 'industuryFlow',
+                    title: "各产业2020年度国内应届生需求",
+                    paddingTop: '8%',
+                    radius: ['35%', '50%'],
+                    radius2: [0, '32%'],
+                    left: '20%',
+                    right: '20%',
+                    top: '0',
+                    bottom: '0',
+                },
+                estate_salary_Data: {},
+                estate_salary_config: {
+                    type: 'natureFlow',
+                    title: '各产业2020年度外籍人才需求',
+                    paddingTop: '8%',
+                    echartTitleShow: false,
+                    left: '4%',
+                    right: '10%',
+                    top: '10%',
+                    bottom: '-8%',
+                    bh: 'bh-50',
+                    isClick: false,
+                },
+                estateData: {},
+                estate_config: {
+                    type: 'lsl',
+                    title: '各产业2020年度国留学归国人才需求',
+                    radius: ['40%', '80%'],
+                    paddingTop: '6%',
+                    top: '15%',
+                    right: '8%',
+                    left: '12%',
+                    bottom: '20%',
+                },
+            }
+        },
+        created() {
+            this.init()
+        },
+        mounted() {},
+        methods: {
+            init() {
+                this.$http({
+                    url: this.$http.adornUrl("op=dash&func=getDashData", "XZX"),
+                    method: "post",
+                    data: {
+                        condition: JSON.stringify({
+                            index: 3,
+                            queryKeys: ['B_B11_3']
+                        })
+                    }
+                }).then(({
+                    data
+                }) => {
+                    if (data && data.code == 200) {
+                        this.pie_aData = data.data.bussinessFlow;
+                        this.pie_a_nextData = data.data.industuryFlow;
+                        this.estate_salary_Data = data.data.natureFlow;
+                        this.estateData = data.data.B_B11_3;
+                        // this.ggxq_Data = data.data.D_D1_1;
+                        // this.zfzc_Data = data.data.C_C2_2;
+                        // this.zfzc_Data2 = data.data.C_C2_4;
+                        this.isShow = true;
+                    }
+                });
             },
         }
-    },
-    created() {
-        this.init()
-    },
-    mounted() {},
-    methods: {
-        init() {
-          this.$http({
-              url: this.$http.adornUrl("op=dash&func=getDashData", "XZX"),
-              method: "post",
-              data:{
-                  condition:JSON.stringify({
-                      index: 3,
-                      queryKeys: ['B_B11_3']
-                  })
-              }
-          }).then(({
-              data
-          }) => {
-              if (data && data.code == 200) {
-                this.pie_aData = data.data.bussinessFlow;
-                this.pie_a_nextData = data.data.industuryFlow;
-                this.estate_salary_Data = data.data.natureFlow;
-                this.estateData = data.data.B_B11_3;
-                // this.ggxq_Data = data.data.D_D1_1;
-                // this.zfzc_Data = data.data.C_C2_2;
-                // this.zfzc_Data2 = data.data.C_C2_4;
-                this.isShow = true;
-              }
-          });
-        },
     }
-}
 </script>
 
 <style lang="scss" scoped>
-.my-board {
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    z-index: 5000;
-    background-color: #ccc;
-}
-
-.main {
-    height: 100vh;
-    background: url('../../../assets/img/background.jpg') no-repeat;
-    background-size: 100% 100%;
-    padding: 0% 2.5% 2.5% 3.5%!important;
-}
+    .my-board {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        z-index: 5000;
+        background-color: #ccc;
+    }
+    
+    .main {
+        height: 100vh;
+        background: url('../../../assets/img/background.jpg') no-repeat;
+        background-size: 100% 100%;
+        padding: 0% 2.5% 2.5% 3.5% !important;
+    }
 </style>
